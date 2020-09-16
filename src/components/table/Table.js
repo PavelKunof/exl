9113aaa2b896e3e2868d69f7596258ace05f0939
Pavel Kunof <site59rus@gmail.com>
2020-09-16 10:12:57 +0500

    Table template

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,22 +1,10 @@
 import {ExcelComponent} from "@core/ExcelComponent";
+import {createTable} from "@/components/table/table.template";
 
 export class Table extends ExcelComponent {
     static className = 'table';
 
     toHTML() {
-        return ` <div class="row">
-            <div class="row-info">&nbsp;</div>
-            <div class="column">A</div>
-            <div class="column">B</div>
-            <div class="column">C</div>
-        </div>
-        <div class="row">
-            <div class="row-info">1</div>
-            <div class="row-data">
-                <div class="cell selected" contenteditable="true"></div>
-                <div class="cell" contenteditable="true"></div>
-                <div class="cell" contenteditable="true"></div>
-            </div>
-        </div>`
+        return createTable(25);
     }
 }
